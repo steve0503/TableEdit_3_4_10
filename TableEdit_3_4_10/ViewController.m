8603a1a31b6a66355c744d9c955a2b6ed7104205
steve0503 <leehr76@gmail.com>
2014-01-06 17:51:02 +0900

    TableView_3_4_10
    Done

--- TableEdit_3_4_10/ViewController.m
+++ TableEdit_3_4_10/ViewController.m
@@ -10,9 +10,66 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITableView *table;
+
 @end
 
 @implementation ViewController
+
+- (IBAction)toggleEdit:(id)sender {
+    
+    self.table.editing = !self.table.editing;
+    
+}
+
+
+-(UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    if (indexPath.row %3 == 0) {
+        return UITableViewCellEditingStyleNone;
+    }
+    else if(indexPath.row %3 == 1)
+    {
+        return UITableViewCellEditingStyleDelete;
+        
+    }
+    else{
+    
+        return UITableViewCellEditingStyleInsert;
+    
+    }
+    
+    
+}
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    if (UITableViewCellEditingStyleDelete == editingStyle) {
+        NSLog(@"%d번째 삭제",indexPath.row);
+    }
+    else{
+    
+        NSLog(@"%d번째 추가",indexPath.row);
+    
+    }
+    
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return 10;
+    
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"CELL_ID"];
+    
+    cell.textLabel.text  = [NSString stringWithFormat:@"Cell %d",indexPath.row];
+    
+    return cell;
+
+}
 
 - (void)viewDidLoad
 {
