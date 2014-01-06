b4b1aedb5eaa9d22ad863d78f25c1dc80f07fa7b
steve0503 <leehr76@gmail.com>
2014-01-06 17:28:35 +0900

    Initial Commit

---
+++ TableEdit_3_4_10Tests/TableEdit_3_4_10Tests.m
@@ -0,0 +1,34 @@
+//
+//  TableEdit_3_4_10Tests.m
+//  TableEdit_3_4_10Tests
+//
+//  Created by SDT-1 on 2014. 1. 6..
+//  Copyright (c) 2014년 steve. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TableEdit_3_4_10Tests : XCTestCase
+
+@end
+
+@implementation TableEdit_3_4_10Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
